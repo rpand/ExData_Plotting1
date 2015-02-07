0cb0913ea1ab39bdd99d284813e224df3281ea20
Ravi S Pandey <2ravispandey@gmail.com>
2015-02-06 23:25:43 -0600

    updated comments

--- plot2.R
+++ plot2.R
@@ -1,11 +1,14 @@
 source("./ExData_Plotting1/DataPrep.R")
 
 plot2 <- function(data=NULL) {
+    #if data is not loaded load it
     if(is.null(data))
         data <- load_data()
     
+    #create png file
     png("./ExData_Plotting1/plot2.png", width=400, height=400)
     
+    #Create plot
     plot(data$Time, data$Global_active_power,
          type="l",
          xlab="",
