bce0eb581f3d34819773f45f75634abb56021b22
Ravi S Pandey <2ravispandey@gmail.com>
2015-02-06 23:24:48 -0600

    commets

--- plot1.R
+++ plot1.R
@@ -1,11 +1,14 @@
 source("./ExData_Plotting1/DataPrep.R")
 
 plot1 <- function(data=NULL) {
+    #if data is not loaded load it
     if(is.null(data))
         data <- load_data()
     
+    #create png file
     png("./ExData_Plotting1/plot1.png", width=400, height=400)
-    
+
+    #histogram    
     hist(data$Global_active_power,
          main="Global Active Power",
          xlab="Global Active Power (kilowatts)",
